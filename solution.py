6afc34eee325aecc2e498af97148d2362495e049
r34p3r <danieljmoore123@gmail.com>
2020-08-30 17:49:38 -0700

    updated

--- solution.py
+++ solution.py
@@ -3,31 +3,35 @@
 
 from __future__ import print_function
 def solution(data, n):
-	l = []
-	c = 0
-	num = 0
-	if n == 1:
-		n += 1
-	data1 = sorted(data)
-	data2 = sorted(data)
+	if len(data) < 100:
 
-	for i in data1:
-		for j in data2:
-			if j != i:
-				c = 0
-			if j == i:
-				c += 1
-				if c >= n:
-					num = j
-		if num:
-			l.append(num)
+		l = []
+		c = 0
 		num = 0
+		if n == 1:
+			n += 1
+		data1 = sorted(data)
+		data2 = sorted(data)
 
-	for i in data1:
-		if i in l:
-			data.remove(i)
-	print(*data, sep=",")
+		for i in data1:
+			for j in data2:
+				if j != i:
+					c = 0
+				if j == i:
+					c += 1
+					if c >= n:
+						num = j
+			if num:
+				l.append(num)
+			num = 0
 
+		for i in data1:
+			if i in l:
+				data.remove(i)
+		print(*data, sep=",")
 
-# solution(l, 5)
-solution([5, 10, 15, 10, 7], 1)
+	else:
+		print(" ")
+
+solution([1, 2, 3], 0)
+solution([5, 10, 15, 10, 7, 2,], 1)
